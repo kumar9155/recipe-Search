7c9f275c2db6940d92f6d9206787bb928bc39482
Gaurav <kumar915516@gmail.com>
2023-07-21 10:05:22 +0530

    first commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'd37dd816-8633-424a-af41-029ea1c652d7';
+export const MODAL_CLOSE_SEC = 2.5;
